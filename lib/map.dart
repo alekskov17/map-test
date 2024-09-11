ea40db4382d704c5c9da5af78a4137d2d18b54f8
Александр Ковалев <aleksandrkovalev@MacBook-Pro-Aleksandr.local>
2024-09-11 22:45:39 +0300

    new test map

---
+++ lib/map.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:yandex_maps_mapkit/mapkit.dart' as YMap;
+import 'package:yandex_maps_mapkit/yandex_map.dart';
+import 'package:yandex_maps_mapkit/mapkit_factory.dart';
+
+class MapW extends StatefulWidget {
+  const MapW({super.key});
+
+  @override
+  State<MapW> createState() => _MapWState();
+}
+
+class _MapWState extends State<MapW> {
+  YMap.MapWindow? _mapWindow;
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    mapkit.onStop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Map"),
+        ),
+        body: YandexMap(
+            scale: 4,
+            onMapCreated: (mapWindow) {
+              _mapWindow = mapWindow;
+              mapkit.onStart();
+            }));
+  }
+}
